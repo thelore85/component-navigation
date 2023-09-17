7594a64064ff29809685458d28724c78eaac1c6a
TheLore85 <garofalo.digital@gmail.com>
2023-09-17 15:08:34 +0200

    footer update

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,8 +7,8 @@ const Footer = () => {
     return(
         <section id="footer">
             <div className="wrapper">
-                <p>This web application is property of piqus.it</p>
-                <p>Privacy Policy - Termini e Condizioni - Cookie Policy - info@piqus.it</p>
+                <p>This web application is property of piqus</p>
+                <p>visit <a href="https://github.com/thelore85" target="_blank">github/thelore85</a></p>
             </div>
         </section>
     )
